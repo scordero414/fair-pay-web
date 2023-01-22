7ace87785944b89c24b1caf64bc6114587762a14
Sebastian Cordero Ramirez <scordero414@Sebastians-MacBook-Pro.local>
2023-01-21 20:31:08 -0500

    setup: Add MaterialUI, Add Husky and Lint-staged, Create theme

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/checks',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
---
+++ src/theme/my-theme.ts
@@ -0,0 +1,34 @@
+import { Roboto } from '@next/font/google';
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0B3954',
+      dark: '#FF6663',
+    },
+    secondary: {
+      main: '#BFD7EA',
+      light: '#FEFFFE'
+    },
+    warning: {
+      main: '#E0FF4F'
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
+
+export default theme;
